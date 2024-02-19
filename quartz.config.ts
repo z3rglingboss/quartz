4f32b3a71867ce55df3f7b5980125e8bd14c1b6e
z3rglingboss Laptop <z3rglingboss@gmail.com>
2024-02-19 16:24:32 -0600

    Quartz sync: Feb 19, 2024, 4:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,14 +3,14 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "JWin.Dance (dance, music, and other nerdy musings)",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jwin.dance",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
